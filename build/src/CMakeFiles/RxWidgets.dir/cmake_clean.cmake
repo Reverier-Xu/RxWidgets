fd2ec7ad18569666847c71c96133b1ce84bbf68a
Reverier-Xu <reverier.xu@outlook.com>
2020-07-16 09:05:20 +0800

    commit AnimatedWidgets srcs

---
+++ build/src/CMakeFiles/RxWidgets.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "../lib/libRxWidgets.a"
+  "../lib/libRxWidgets.pdb"
+  "CMakeFiles/RxWidgets_autogen.dir/AutogenUsed.txt"
+  "CMakeFiles/RxWidgets_autogen.dir/ParseCache.txt"
+  "RxWidgets_autogen"
+  "CMakeFiles/RxWidgets.dir/RxWidgets_autogen/mocs_compilation.cpp.o"
+  "CMakeFiles/RxWidgets.dir/animatedwidget.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/RxWidgets.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/src/CMakeFiles/RxWidgets.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for RxWidgets.
+# This may be replaced when dependencies are built.
